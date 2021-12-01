1234540f36309459ada0b79d674ce1e9dd801a9a
santosh463 <santoshnaidu128@gmail.com>
2021-12-01 11:50:54 +0000

    assetmonk

---
+++ src/components/Cart/index.js
@@ -0,0 +1,86 @@
+import {Component} from 'react'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css'
+import Header from '../Header'
+import './index.css'
+
+class Cart extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      imagesList: [],
+      photoIndex: 0,
+      isOpen: false,
+    }
+  }
+
+  componentDidMount() {
+    this.getAlbums()
+  }
+
+  getAlbums = async () => {
+    const apiUrl = `https://jsonplaceholder.typicode.com/photos`
+    const options = {
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.map(item => ({
+        title: item.title,
+        url: item.url,
+        id: item.id,
+        thumbnailUrl: item.thumbnailUrl,
+      }))
+      this.setState({
+        imagesList: updatedData,
+      })
+    }
+  }
+
+  render() {
+    const {photoIndex, isOpen, imagesList} = this.state
+
+    return (
+      <>
+        <Header />
+        <div className="album-container">
+          <button
+            className="albums"
+            type="button"
+            onClick={() => this.setState({isOpen: true})}
+          >
+            Open Album
+          </button>
+
+          {isOpen && (
+            <Lightbox
+              mainSrc={imagesList[photoIndex]}
+              nextSrc={imagesList[(photoIndex + 1) % imagesList.length]}
+              prevSrc={
+                imagesList[
+                  (photoIndex + imagesList.length - 1) % imagesList.length
+                ]
+              }
+              onCloseRequest={() => this.setState({isOpen: false})}
+              onMovePrevRequest={() =>
+                this.setState({
+                  photoIndex:
+                    (photoIndex + imagesList.length - 1) % imagesList.length,
+                })
+              }
+              onMoveNextRequest={() =>
+                this.setState({
+                  photoIndex: (photoIndex + 1) % imagesList.length,
+                })
+              }
+            />
+          )}
+        </div>
+      </>
+    )
+  }
+}
+
+export default Cart
---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,128 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    showErrorMsg: false,
+    errorMsg: '',
+  }
+
+  onSubmitSuccess = () => {
+    const {history} = this.props
+    history.push('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({
+      showErrorMsg: true,
+      errorMsg,
+    })
+  }
+
+  // eslint-disable-next-line consistent-return
+  submitForm = async event => {
+    event.preventDefault()
+    const {email, password} = this.state
+
+    const data = {
+      email,
+      password,
+    }
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: 'Bearer ACCESS-TOKEN',
+      },
+      body: JSON.stringify(data),
+    }
+
+    const res = await fetch('https://reqres.in/api/register', options)
+      .then(response => response.json())
+      .then(jsonData => {
+        console.log(jsonData)
+      })
+    if (res === true) {
+      this.onSubmitSuccess()
+    }
+  }
+
+  onChangeUsername = event => {
+    this.setState({email: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-filed"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  renderEmailField = () => {
+    const {email} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="email">
+          EMAIL
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="username-input-filed"
+          value={email}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  render() {
+    const {showErrorMsg, errorMsg} = this.state
+    const {history} = this.props
+    return (
+      <div className="login-form-container">
+        <form className="form-container" onSubmit={this.submitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
+            alt="website logo"
+          />
+          <div className="input-container">{this.renderEmailField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <button
+            type="submit"
+            className="login-button"
+            onClick={() => {
+              history.push('/')
+            }}
+          >
+            Login
+          </button>
+          {showErrorMsg && <p className="error-message">*{errorMsg}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
